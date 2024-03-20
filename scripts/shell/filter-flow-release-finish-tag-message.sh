0d7b90fdccb43f7b5a4ae7380e5b76004f53ad26
kuangzhibin <1520257238@qq.com>
2024-03-20 10:03:45 +0800

    feat(mandev-react): 初始化项目结构(changelog-needed)
    ISSUES CLOSED: none

---
+++ scripts/shell/filter-flow-release-finish-tag-message.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Runs during git flow release finish and a tag message is given
+#
+# Positional arguments:
+# $1 Message
+# $2 Full version
+#
+# Return MESSAGE
+#
+# The following variables are available as they are exported by git-flow:
+#
+# MASTER_BRANCH - The branch defined as Master
+# DEVELOP_BRANCH - The branch defined as Develop
+
+MESSAGE=$1
+VERSION=$2
+# 根据tag来截取需要写入日志的package
+PACKAGE=${VERSION#*-}
+# 获取需要写入日志的package最近的一个tag
+PREVIOUSTAG=$(git tag -l | grep $PACKAGE | tail -n 1)
+# 获取semver格式的版本号
+PACKAGE_VERSION=${VERSION%%-*}
+
+# 获取两个tag之间的changelog信息
+CHANGELOG_MESSAGE=`yarn cross-env PACKAGE=$PACKAGE PREVIOUS_TAG=$PREVIOUSTAG CURRENT_TAG=$VERSION  conventional-changelog -p custom-config -i -n ./scripts/changelog/changelog-option.cjs | tail -n +4 | sed '$d' | sed 's/(changelog-needed)/ /g'`
+
+# 判断是否需要rebase，落后于target branch合并会失败
+[ $BEHIND_COMMIT -ne 0 ] && { echo 'Please rebase master before finishing this branch'; exit 1; }
+
+TEMP_CHANGELOG_MESSAGE=$(echo "### $PACKAGE_VERSION";git log -1 --pretty="#### %ci";printf "\n";echo "${CHANGELOG_MESSAGE}";printf "\n---\n\n";cat ./CHANGELOG.md)
+echo "$TEMP_CHANGELOG_MESSAGE" > ./CHANGELOG.md
+
+git add . > /dev/null
+git commit --amend --no-edit --no-verify > /dev/null
+
+echo $MESSAGE
+
+exit 0
